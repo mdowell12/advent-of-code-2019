8eac645dd9eadf8ee5ae3850f36a5d4e347a7248
Matt Dowell <mattdowell12@gmail.com>
2021-01-18 16:33:11 -0600

    Move inputs folder inside solutions folder

--- solutions/src/util.rs
+++ solutions/src/util.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
 pub fn read_inputs(day: i8) -> Vec<String> {
-    let path = format!("../inputs/{}.txt", day);
+    let path = format!("./inputs/{}.txt", day);
     let input = fs::read_to_string(path)
         .expect(&format!("Something went wrong reading the file for day {}", day).to_string());
 
